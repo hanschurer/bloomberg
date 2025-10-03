14e9ce6a83e417251540e8f4b04001602c0291e4
JodySheng <sheng.fe@northeastern.edu>
2025-10-03 15:40:49 -0700

    add 42, 611

---
+++ 611_Valid Triangle Number/main.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/valid-triangle-number/description/
+
+class Solution:
+    def triangleNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        nums.sort()
+        ans = 0
+        for i in range(n):
+            k = i
+            for j in range(i + 1, n):
+                while k + 1 < n and nums[k + 1] < nums[i] + nums[j]:
+                    k += 1
+                ans += max(k - j, 0)
+        return ans
