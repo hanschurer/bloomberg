93086889881d5fb54fc88a077e62899cc8714b3b
Immortal-Dream <tonyliu02@outlook.com>
2025-10-04 00:41:52 -0400

    Problem 1131 on LeetCode, which is a variant of a real interview question from Rednote

---
+++ 1161_Maximum Level Sum of a Binary Tree/Main.py
@@ -0,0 +1,9 @@
+'''
+source: https://www.xiaohongshu.com/explore/68e046a5000000000700c6ed?app_platform=ios&app_version=9.3.1&share_from_user_hidden=true&xsec_source=app_share&type=normal&xsec_token=CB9EJWt5qEzdbGv5Y6tcYFeu6RkChW6VYt4TJqQMamHMc=&author_share=1&xhsshare=WeixinSession&shareRedId=N0k4NkVLNjo2NzUyOTgwNjczOTo0RkpC&apptime=1759530496&share_id=bf29fe0a02e342feb23979d30404a864
+similar question
+Given the root of a binary tree, the level of its root is 1, the level of its children is 2, and so on.
+
+Return the smallest level x such that the sum of all the values of nodes at level x is maximal.
+
+BFS + counting
+'''
