aaf2221b92ca39c3ecfe4e25d5aadc86037011fd
Immortal-Dream <tonyliu02@outlook.com>
2025-10-11 22:54:13 -0400

    add question from 1point3acres

---
+++ Minimum Steps to Reach Target/main.py
@@ -0,0 +1,26 @@
+'''
+source: https://www.1point3acres.com/bbs/thread-1023558-1-1.html
+You start with the integer 1. At each step, you may perform exactly one of the following operations:
+    n=n×2
+    n=n÷3
+
+Given a target integer tt, determine the minimum number of steps required to reach tt starting from 1.
+
+
+# Example 1
+Input: t = 2
+Steps: 1 -> 2
+Output: 1
+
+
+# Example 2
+Input: t = 10
+One possible path: 1 -> 2 -> 4 -> 8 -> 16 -> 5 -> 10
+Steps = 6
+Output: 6
+
+# Example 3
+Input: t = 1
+Already at target
+Output: 0
+'''
