578b2ef3e55cd28324fd6817cacdd185b53c4034
Han Wang <hanwangx@gmail.com>
2025-10-12 22:50:51 -0700

    add some variant description

--- 1656. Design an Ordered Stream/main.py
+++ 1656. Design an Ordered Stream/main.py
@@ -1,6 +1,10 @@
 # https://leetcode.com/problems/design-an-ordered-stream/description/
+# Input: (1, "abcd"), (2, "efgh"), (4, "mnop"), (5, "qrst"), (3, "ijkl")
 
-class OrderedStream:
+# Write a program to output the data from the stream in realtime in order, so 1,2,3,4,5..
+# You cannot queue up the incoming data from the stream.
+# So for example if the first incoming bit of data is (1, "abcd"), and the second is (4, "mnop"), you cannot output (4, "mnop") until you get 2, 3.
+# class OrderedStream:
 
     def __init__(self, n: int):
         self.data = [None]*n
