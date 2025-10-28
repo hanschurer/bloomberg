19face505650654b6d5e10f48dced3601242fcfb
Justin Xiao <justinxiaoxulong@gmail.com>
2025-10-27 22:20:39 -0700

    update re-order array based on dict

--- Re-order Array Based on Dictionary/main.py
+++ Re-order Array Based on Dictionary/main.py
@@ -13,3 +13,51 @@
 # [<Drax, Engineer>, <Daniel, Engineer>, <John, Manager>, <Sally, CTO>, <Bob, CFO>, <Sam, CEO>]
 
 # Note that in this case, CTO and CFO both report to CEO so they are both before CEO and above the next biggest thing, which is manager. They can also be in either order in this case.
+
+from collections import defaultdict, deque
+from typing import Dict, List, Tuple, Any
+
+
+def solution(employees: List[Tuple[str, str]], reports: Dict[str, Any]) -> List[Tuple[str, str]]:
+    # find ordering based on reports w/ topo sort
+    ind = {}
+    graph = defaultdict(list)
+    for k, v in reports.items():
+        graph[v].append(k)
+        ind.setdefault(k, 0)
+        ind.setdefault(v, 0)
+        ind[k] += 1
+
+    # find ordering from CEO, reverse the order
+    orders = {}
+    order = len(ind)
+    q = deque(title for title, indegree in ind.items() if not indegree)
+    while q:
+        for _ in range(len(q)):
+            cur_title = q.popleft()
+            orders[cur_title] = order
+            for n_title in graph[cur_title]:
+                ind[n_title] -= 1
+                if not ind[n_title]:
+                    q.append(n_title)
+        order -= 1
+
+    return sorted(employees, key=lambda x: (orders[x[1]]))
+
+
+employees = [
+    ("John", "Manager"),
+    ("Sally", "CTO"),
+    ("Sam", "CEO"),
+    ("Drax", "Engineer"),
+    ("Bob", "CFO"),
+    ("Daniel", "Engineer")
+]        
+reports = {
+    "CTO": "CEO",
+    "Manager": "CTO",
+    "Engineer": "Manager",
+    "CFO": "CEO"
+}
+print(solution(employees, reports))
+
